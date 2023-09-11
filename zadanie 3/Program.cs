76152152ea7a639172c191956591ec1508b31e45
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-11 13:12:20 +0300

    Добавьте файлы проекта.

---
+++ zadanie 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задание 3");
+        AllInfoProcess();
+    }
+    static void AllInfoProcess()
+    {
+        var myProcess = from proc in Process.GetProcesses(".")
+                        orderby proc.BasePriority
+                        select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n");
+        foreach (var p in myProcess)
+            Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+        
+    }
+}
